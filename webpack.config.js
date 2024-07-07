5a3f207fbfc5b98171737fde2a9e9181b5b07f2e
Stewie1 <xionone@web.de>
2024-07-07 17:55:41 +0200

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', /* template nicht vergessen amigo :)) */
+            title: 'ToDoList',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        watchFiles: {
+            paths: ['src/index.html', 'src/index.js', 'src/style.css'],
+            options: {
+                usePolling: false,
+            },
+        },
+        compress: true,
+        port: 9000,
+    },
+};

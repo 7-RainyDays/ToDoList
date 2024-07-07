82f713a2ca1e13705a76b451c016a3dbffe5caed
Stewie1 <xionone@web.de>
2024-07-07 19:38:55 +0200

    basic structure implemented

---
+++ src/project.js
@@ -0,0 +1,18 @@
+class Project {
+
+    constructor() {
+        this.title = title;
+        this.description = description;
+    }
+}
+
+
+class ToDoList {
+
+    constructor() {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}

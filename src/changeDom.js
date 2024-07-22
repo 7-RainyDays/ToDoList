d7f4c776a3dcac6bb6e860b63642eea0e13f7607
Stewie1 <xionone@web.de>
2024-07-22 09:52:22 +0200

    added functionality to edit todos, fixed bug not showing todos after project switch

--- src/changeDom.js
+++ src/changeDom.js
@@ -1,18 +1,23 @@
-//display todos funktioniert noch nicht richtig:;
-
 import { Project, Todo } from "./project";
 
 
 const domHandler = () => {
 
     let currentProject = "";
-    const projectArray = []; //Array Brackets != Object Brackets;
+    let currentTodo = "";
+    const projectArray = [];
     const defaultProject = new Project('1', 'Your First Project', 'I got things to do.')
     projectArray.push(defaultProject);
 
     //dialogs
     const dialogNewProject = document.getElementById('add-project-dialog');
     const dialogAddTodo = document.getElementById('add-todo-dialog');
+    const dialogEditTodo = document.getElementById('edit-todo-dialog');
+
+    //form
+    const proForm = document.getElementById('project-form');
+    const todoForm = document.getElementById('todo-form');
+    const editTodoForm = document.getElementById('edit-todo-form');
 
     //left section 
     const allProjectsDiv = document.querySelector('.all-projects-con');
@@ -21,6 +26,7 @@ const domHandler = () => {
     const btnNewProject = document.querySelector('.add-project-btn');
     const btnsSubmitPro = document.getElementsByClassName('confirm-project');
     const btnsSubmitTodo = document.getElementsByClassName('confirm-todo');
+    const btnsSubmitEditedTodo = document.querySelector('.confirm-edit-todo');
     const btnsCancel = document.getElementsByClassName('cancel');
 
     //right Section
@@ -29,11 +35,24 @@ const domHandler = () => {
     const rightTodos = document.querySelector('.container-todos');
     const listTodos = document.querySelector('.to-do-list');
 
+    //edit Todo Dialog Values
+    const editTodoName = document.getElementById('edit-todo-name');
+    const editTodoDueDate = document.getElementById('edit-due-date');
+    const editPriority = document.getElementById('edit-priority');
+    const editNotes = document.getElementById('edit-notes');
+
     //DOM MANIPULATION
 
     const displayRightSide = (proj) => {
         children[0].innerHTML = proj.title;
         children[1].innerHTML = proj.description;
+    };
+
+    const resetTodoInDOM = () => {
+        const getList = document.getElementsByClassName('todo');
+        if (getList.length > 0) {
+            Array.from(getList).forEach(element => element.remove());
+        };
     };
 
     //display projects on DOM
@@ -78,47 +97,94 @@ const domHandler = () => {
     };
 
     const displayTodos = (todoArray) => {
-        todoArray.forEach(todo => {
+        resetTodoInDOM();
+        for (let i = 0; i < todoArray.length; i++) {
+            const todoValues = Object.values(todoArray[i]);
+            const todoKeys = Object.keys(todoArray[i]);
             const listItem = document.createElement('li');
-            listItem.setAttribute('class', todo);
-            listItem.innerHTML = `Name: ${todo.name} , Due Date: ${todo.dueDate}, Priority: ${todo.dueDate}, Notes: ${todo.notes}`;
+            listItem.setAttribute('class', 'todo');
 
-            const deleteTodo = document.createElement('Button')
-            deleteTodo.setAttribute('type', 'button');
-            deleteTodo.setAttribute('class', 'delete');
-            deleteTodo.innerHTML = 'delete';
+            for (let y = 0; y < todoValues.length; y++) {
+                const p = document.createElement('p');
+                p.innerHTML = todoValues[y]
+                p.classList.add(todoKeys[y])
+                listItem.appendChild(p);
+            }
 
-            const editTodo = document.createElement('Button')
-            editTodo.setAttribute('type', 'button');
-            editTodo.setAttribute('class', 'delete');
-            editTodo.innerHTML = 'edit';
+            const toBeDeleted = document.createElement('Button')
+            toBeDeleted.type = 'button';
+            toBeDeleted.classList.add('delete');
+            toBeDeleted.innerHTML = 'delete';
 
-            listItem.appendChild(deleteTodo);
-            listItem.appendChild(editTodo);
+            //hier removeTodoDOM(i) führt zu einem sofortigen Funktionsaufruf, und schließlich zum Fehler
+            //problematisch: .addEventListener("click", () => removeTodoDOM(i));
+            //anonyme Funktion () besser, da diese erst beim Click ausgeführt wird
+            toBeDeleted.addEventListener("click", () => removeTodoDOM(i));
+
+            const toBeEdited = document.createElement('Button')
+            toBeEdited.type = 'button';
+            toBeEdited.classList.add('edit');
+            toBeEdited.innerHTML = 'edit';
+            toBeEdited.addEventListener("click", () => editTodo(i));
+
+            listItem.appendChild(toBeDeleted);
+            listItem.appendChild(toBeEdited);
             listTodos.appendChild(listItem);
+        };
+    };
+
+    const editTodo = (i) => {
+        currentTodo = i;
+        editTodoName.value = currentProject.todoArray[i].name;
+        editTodoDueDate.value = currentProject.todoArray[i].dueDate;
+        editPriority.value = currentProject.todoArray[i].priority;
+        editNotes.value = currentProject.todoArray[i].notes;
+        dialogEditTodo.showModal();
+    };
+
+    (function submitEditedTodo() {
+        btnsSubmitEditedTodo.addEventListener('click', () => {
+            const i = currentTodo;
+            const editTodoName = document.getElementById('edit-todo-name').value.trim();
+            const date = document.getElementById('edit-due-date').value.trim();
+            const prio = document.getElementById('edit-priority').value.trim();
+            const note = document.getElementById('edit-notes').value.trim();
+
+            currentProject.todoArray[i].name = todoName;
+            currentProject.todoArray[i].dueDate = date;
+            currentProject.todoArray[i].priority = prio;
+            currentProject.todoArray[i].notes = note;
+
+            dialogEditTodo.close('submitted edited Todo')
+            editTodoForm.reset();
+            displayTodos(currentProject.todoArray);
         });
-    }
+    })();
 
-    const cleanTodoList =
+    const removeTodoDOM = (i) => {
+        const getList = document.getElementsByClassName('todo');
+        Array.from(getList)[i].remove();
+        currentProject.deleteTodo(i);
+    };
 
-        //EVENT LISTENERS 
+    //EVENT LISTENERS 
 
-        //logic to add todos with dialog
-        (function addSubmitTodoListener() {
-            Array.from(btnsSubmitTodo).forEach(todo => {
+    //logic to add todos with dialog
+    (function addSubmitTodoListener() {
+        Array.from(btnsSubmitTodo).forEach(todo => {
 
-                todo.addEventListener('click', event => {
-                    const respectiveProject = event.target.closest('.project')//.getAttribute('data-project-id');
-                    const todoName = document.getElementById('todo-name').value.trim();
-                    const todoDueDate = document.getElementById('due-date').value.trim();
-                    const priority = document.getElementById('priority').value.trim();
-                    const notes = document.getElementById('notes').value.trim();
-                    currentProject.initializeTodo(todoName, todoDueDate, priority, notes);
-                    dialogAddTodo.close('submitted new Todo')
-                    displayTodos(currentProject.todoArray);
-                });
+            todo.addEventListener('click', () => {
+                const todoName = document.getElementById('todo-name').value.trim();
+                const todoDueDate = document.getElementById('due-date').value.trim();
+                const priority = document.getElementById('priority').value.trim();
+                const notes = document.getElementById('notes').value.trim();
+                currentProject.initializeTodo(todoName, todoDueDate, priority, notes);
+                dialogAddTodo.close('submitted new Todo')
+                todoForm.reset();
+                displayTodos(currentProject.todoArray);
             });
-        })();
+        });
+    })();
 
     //handle add project button 
     const btnAddProject = () => {
@@ -127,6 +193,7 @@ const domHandler = () => {
         });
     };
 
+    //logik fehlt noch
     const updateCurrentProject = (id) => {
         currentProject = projectArray.find((pro) => pro.projectID === id);
     }
@@ -146,12 +213,14 @@ const domHandler = () => {
                         break;
                     case 'delete-project':
                         alert('delete');
+                        toBeEdited.classList.add('edit');
                         break;
                 };
             } else {
                 displayRightSide(currentProject);
-            }
-        })
+                displayTodos(currentProject.todoArray);
+            };
+        });
     })();
 
     //add cancel button listener
@@ -160,8 +229,8 @@ const domHandler = () => {
             btn.addEventListener("click", event => {
                 const dialog = event.currentTarget.closest('dialog');
                 dialog.close("cancel action");
-            })
-        })
+            });
+        });
     })();
 
     //add submit Project button listener
@@ -177,6 +246,7 @@ const domHandler = () => {
                 projectArray.push(newProject);
                 displayNewProjects(newProject);
                 dialogNewProject.close("confirmed entry");
+                proForm.reset();
             })
         });
     }
